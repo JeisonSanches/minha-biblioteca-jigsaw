d90639a8b052ee643025a981614dd50401cb1ff9
Serena <sazuna@users.noreply.github.com>
2017-05-17 20:32:34 -0300

    Moving cookie value to separate file

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ $ pip install -r requirements.txt
 ```
 
 ##### Utilização
-1) Através do SIGA3, acesse o site MinhaBiblioteca via Biblioteca > E-books MinhaBiblioteca 
+1) Através do SIGA3, acesse o site MinhaBiblioteca via Biblioteca > E-books MinhaBiblioteca
 2) Abra o Developer's Console (F12?) na aba Network
 3) Selecione o livro que você quer baixar
 4) Olhe os últimos requests, ignore os \*.svg e procure por `pages` (ou figures, ou bookmarks, ou...)
 5) Verifique a Request URL, tem parecer com essa: `https://jigsaw.vitalsource.com/api/v0/books/9788582601044/pages`
-6) Na URL que você encontrou, procure nos Reponse Headers pelo parâmetro Set-Cookie. Copie **todo** o valor dele e coloque na variável `starting_cookie`. Sim, é enorme. Se faltar algum caractere não vai funcionar. O cookie que você obtém aqui vale por algumas horas, então não é preciso realizar esse passo todas as vezes.
+6) Na URL que você encontrou, procure nos Reponse Headers pelo parâmetro Set-Cookie. Copie **todo** o valor dele e coloque no arquivo cookie.txt. Sim, é enorme. Se faltar algum caractere não vai funcionar. Se houver caracteres extras não vai funcionar. O cookie que você obtém aqui vale por algumas horas, então não é preciso realizar esse passo todas as vezes.
 7) No Request URL, o número que segue `books/` é o ISBN do livro. Use no próximo passo.
 8) `python main.py :isbn`
 9) Se o livro estiver em HTML, será criada uma pasta com o timestamp atual no diretório de execução. Abra o livro em `./[timestamp]/Text/[book_title].html`. Se o livro for um conjunto de imagens, será criado o PDF `./[book_title].pdf`
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 import sys
 from shutil import copyfileobj, rmtree
 from os import mkdir, listdir, makedirs, path
-from urllib.parse import urljoin 
+from urllib.parse import urljoin
 from datetime import datetime
 from fpdf import FPDF
 import json
@@ -15,9 +15,9 @@ import json
 Primeiro cookie tem que ser obtido manualmente. Abra MinhaBiblioteca pelo SIGA normalmente.
 No browser abra o developers console e faça um request para https://jigsaw.vitalsource.com/books/9788582601044
 Olhe o último request que termina com um número (".../encrypted/1600" por exemplo). Nos response headers,
-vai ter um parâmetro Set-Cookie. Copie e cole aqui o valor.
+vai ter um parâmetro Set-Cookie. Copie e cole o valor no arquivo cookie.txt
 """
-starting_cookie = "_jigsaw_session=ODFpa293UWlwK00xVXFJWUgwSTZob2JZZkhTdGJPRGEyMyttK3BXcWoyaitocHc3QXhLUHFpUkJ5dXZXZ3hxYjFFNjhBMUl4WDVFUElpN0hTN3c1SnZEVGxybFlrdEtYbFQwbkV6eDFsUXRQbnFjazl3ZGJwaTJWV2swdnNwcG0rN1J3Q250ZnZ5Si9rMG1UZk80aUxNbWpLN1lmMTVFcTlSbFVMRlcyUmZUaDBmS3FYV3REVWkzdEYvTEpPZSs2MU9neVFSTTZvWTMyV2lwWk4vMGZIa0RxWTk3cmprZGU0Ky9QamtTUzQ1bG1GUmFtV0I5UEppNUMyTHFhc2RYZ2tCT1FKeXIwWTZkRDkyN25YcVVTZ05Xb0o1TVMxTjVZZUFRUkEycG1rSjBPcUdmWEVsaG1Hc0RZVFR2dGovZVBZMnZiSDY3UVA5YmpQd2I2OWZMRDUrSTJLNGZNNVMyVW15RGUwTWN3cjNreGI2VWJuUG1hc3dYQjdneUc3TERYaUF3OWdoOW92MEZMSEtSdnVJVFNCWHN1UWVId0wxL21SWmg0c0ZUTDFKOWdNM0pqaU9YVTZsd3ViRlNESXAvR2JFWmxVbDg4SU12N2UxMTBka2xhQkRGNE5hNlptTzIvalRuOThEYmtQelVSY3l6TWZjQlFuVUptMGNCbmxCaEktLWlsc2FrZnZneUtMVnl5TVgxUnVpbUE9PQ%3D%3D--23c103e480047bdc023a0f306f9ebf841573ecd7; path=/; HttpOnly"
+starting_cookie = None
 
 
 def main(_id=None):
@@ -31,11 +31,12 @@ def main(_id=None):
     _id = _id or "9788537803868"
     metadata =  get_metadata(_id)
     title = metadata['title'] if metadata else None
+    print("Book title is: " + title if title else "I couldn't download the metadata")
     url = "https://jigsaw.vitalsource.com/api/v0/books/{}/pages".format(_id)
     filename = write_api_response(url)
     download_from_api_response(filename, title)
     #download_from_api_response('out2.json')
-    
+
 """
 Most of these are probably unnecessary, but I'm too lazy to find out which.
 """
@@ -44,17 +45,15 @@ page_headers = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate, sdch, br',
     'Accept-Language':'en-US,en;q=0.8',
-    'Host':'jigsaw.vitalsource.com',
-    'Cookie': starting_cookie
+    'Host':'jigsaw.vitalsource.com'
 }
-    
+
 image_headers = {
     'User-Agent': UserAgent().chrome,
     'Accept':'image/webp,image/*,*/*;q=0.8',
     'Accept-Encoding':'gzip, deflate, sdch, br',
     'Accept-Language':'en-US,en;q=0.8',
-    'Host':'jigsaw.vitalsource.com',
-    'Cookie': starting_cookie
+    'Host':'jigsaw.vitalsource.com'
 }
 
 api_headers = {
@@ -64,8 +63,7 @@ api_headers = {
     'Accept-Language':'en-US,en;q=0.8',
     'Host':'jigsaw.vitalsource.com',
     'Referer':'https://jigsaw.vitalsource.com/books/9788582601044/pages/recent?jigsaw_brand=integradaminhabiblioteca&xdm_e=https%3A%2F%2Fintegrada.minhabiblioteca.com.br&xdm_c=default4014&xdm_p=1',
-    'X-Requested-With':'XMLHttpRequest',
-    'Cookie':starting_cookie
+    'X-Requested-With':'XMLHttpRequest'
 }
 
 def get_metadata(_id):
@@ -80,9 +78,8 @@ def get_metadata(_id):
         match = re.search("VST.currentBookData.*(\{.*\});",script.get_text())
         if match:
             return json.loads(match.group(1))
-    print("Couldn't find metadata")
     return None
-    
+
 def download_from_api_response(source, book_title):
     baseurl = "https://jigsaw.vitalsource.com"
     with open(source) as f:
@@ -90,14 +87,14 @@ def download_from_api_response(source, book_title):
     pdf = FPDF(orientation='P',format='A4')
     pdf.set_auto_page_break(False)
     epub = '<head><meta charset="UTF-8"></head>'
-    
+
     """
     If you download the same book twice I will remove the old files. Back it up.
     Or don't I'm not your boss.
     """
     dirname = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     mkdir(dirname)
-    
+
     session = requests.Session()
     is_epub = False
     for index, page in enumerate(pages):
@@ -131,27 +128,27 @@ def download_from_api_response(source, book_title):
             Book pages are images
             """
             response = session.get(url, headers=page_headers)
-            
+
             soup = BeautifulSoup(response.content,'html.parser')
             pageurl = soup.find('img',id='pbk-page')['src']
-            
+
             image_headers['Cookie'] = response.headers['Set-Cookie']
             page_headers['Cookie'] = response.headers['Set-Cookie']
-            
+
             image_headers['Referer'] = url
             image_response = session.get(baseurl + pageurl,headers=image_headers, stream=True)
-            
+
             image_headers['Cookie'] = image_response.headers['Set-Cookie']
             page_headers['Cookie'] = image_response.headers['Set-Cookie']
-            
+
             filename = str(page['number']).zfill(3) + "." + path.split(image_response.headers['content-type'])[1]
             with open(path.join(dirname,filename), 'wb') as out:
                 copyfileobj(image_response.raw, out)
-                
+
             pdf.add_page()
             pdf.image(path.join(dirname,filename),x=0,w=210,h=297)
         print("{}/{}".format(index+1,len(pages)))
-        
+
     if is_epub:
         epub_name = "{}/Text/{}.html".format(dirname,book_title)
         makedirs(path.dirname(epub_name))
@@ -165,14 +162,23 @@ def download_from_api_response(source, book_title):
         print("PDF title: {}".format(pdf_name))
         pdf.output(pdf_name, "F")
     print("Done. Yes, I am that cool.")
-        
+
 def write_api_response(url):
     response = requests.get(url, headers=api_headers)
     filename = "out.json"
     with open(filename,'w+') as f:
         f.write(json.dumps(response.json()))
+    print("Got everything I need. Starting download...")
     return filename
-    
+
+import argparse
+
 if __name__ == "__main__":
+    if not path.isfile('cookie.txt'):
+        print("Coloque o cookie inicial no arquivo cookie.txt")
+    with open('cookie.txt') as f:
+        starting_cookie = f.read()
+        page_headers['Cookie'] = starting_cookie
+        api_headers['Cookie'] = starting_cookie
     _id = sys.argv[1] if len(sys.argv) > 1 else None
     main(_id)
